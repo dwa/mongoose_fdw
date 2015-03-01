7e7d0c1533a1d2effcc6cb5e623926901d34f638
David Wallin <dwa@havanaclub.org>
2015-03-01 23:09:19 +0100

    better product description

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 ###
 ### Author: David Wallin
-### Time-stamp: <2015-03-01 17:29:15 dwa>
+### Time-stamp: <2015-03-01 23:05:12 dwa>
 
 from setuptools import setup
 
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     setup(name='mongoose_fdw',
           author='David Wallin',
           author_email='dwa@havanaclub.org',
-          description='Alternative Postgres fdw for MongoDB',
+          description='An experimental Postgres fdw for MongoDB',
           url='http://github.com/dwa/mongoose_fdw',
           version='0.0.1',
           install_requires=['pymongo'],
