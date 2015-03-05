6984447d78f597009eeed538cd184a4d191589e3
David Wallin <dwa@havanaclub.org>
2015-03-05 21:58:43 +0100

    allow nested fields

--- mongoose_fdw/__init__.py
+++ mongoose_fdw/__init__.py
@@ -1,12 +1,17 @@
 ###
 ### Author: David Wallin
-### Time-stamp: <2015-03-04 23:06:35 dwa>
+### Time-stamp: <2015-03-05 21:54:14 dwa>
 
 from multicorn import ForeignDataWrapper
 from multicorn.utils import log_to_postgres as log2pg
 
 from pymongo import MongoClient
 from dateutil.parser import parse
+
+from functools import partial
+
+dict_traverser = partial(reduce,
+                         lambda x, y: x.get(y, {}) if type(x) == dict else x)
 
 
 def coltype_formatter(coltype):
@@ -42,7 +47,9 @@ class Mongoose_fdw (ForeignDataWrapper):
         self.db = getattr(self.c, self.db_name)
         self.coll = getattr(self.db, self.collection_name)
 
-        self.fields = {col: coltype_formatter(coldef.type_name) for (col, coldef) in columns.items()}
+        # log2pg('cols: {}'.format(columns))
+        self.fields = {col: {'formatter': coltype_formatter(coldef.type_name),
+                             'path': col.split('.')} for (col, coldef) in columns.items()}
 
     def build_spec(self, quals):
         Q = {}
@@ -53,15 +60,16 @@ class Mongoose_fdw (ForeignDataWrapper):
                        '<': '$lt'}
 
         for qual in quals:
-            val_formatter = self.fields[qual.field_name]
+            val_formatter = self.fields[qual.field_name]['formatter']
             vform = lambda val: val_formatter(val) if val_formatter is not None else val
             if qual.operator == '=':
-
                 Q[qual.field_name] = vform(qual.value)
+
             elif qual.operator in ('>', '>=', '<=', '<'):
                 comp = Q.setdefault(qual.field_name, {})
                 comp[comp_mapper[qual.operator]] = vform(qual.value)
                 Q[qual.field_name] = comp
+
             else:
                 log2pg('Qual operator {} not implemented yet: {}'.format(qual.field_name, qual))
         return Q
@@ -74,10 +82,12 @@ class Mongoose_fdw (ForeignDataWrapper):
             fields['_id'] = False
 
         Q = self.build_spec(quals)
-
+        # log2pg('spec: {}'.format(Q))
+        # log2pg('fields: {}'.format(fields))
         cur = self.coll.find(spec=Q, fields=fields, snapshot=True)
         for doc in cur:
-            yield doc
+            yield {col: dict_traverser(self.fields[col]['path'], doc) for col in columns}
+
 
 ## Local Variables: ***
 ## mode:python ***
